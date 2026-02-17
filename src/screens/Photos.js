3033e261b5a9f9431335d64169362cda1b88eaab
S263100 <S263100@uos.ac.uk>
2026-02-17 15:17:53 +0000

    Added photos and stylying for display.

--- src/screens/Photos.js
+++ src/screens/Photos.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, FlatList, Image } from 'react-native';
 import { global } from '../config/global';
 
 export default function PhotosScreen({ navigation }) {
@@ -30,7 +30,18 @@ export default function PhotosScreen({ navigation }) {
 
         return (
             <View style={styles.PhotosScreen}>
-                <Text>Photos Screen</Text>
+                {photos && photos.length > 0 ? (<View style={styles.resultsContainer}>
+                        <FlatList
+                        data={photos}
+                        numColumns={2}
+                        style={{margin: 10}}
+                        renderItem={({item}) => (
+                            <Image style={styles.resultImage} source={{uri: item.urls.regular}}/>)}
+                        />
+                </View>) : (<View style={styles.loadingContainer}>
+                     <ActivityIndicator size="large" color="#000"/>
+                     </View>
+                     )}
             </View>
         );
 }
@@ -38,5 +49,14 @@ export default function PhotosScreen({ navigation }) {
 const styles = StyleSheet.create({
     PhotosScreen: {
         /* styles here */
+    },
+    loadingContainer: {
+        height: '100%',
+        justifyContent: 'center'
+    },
+    resultImage: {
+        flex: 1,
+        margin: 10,
+        height: 200
     },
 });
