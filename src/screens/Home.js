bea1fc0461cdeab224aa9246b06d75cdada2a4e8
S263100 <S263100@uos.ac.uk>
2026-03-12 09:57:27 +0000

    Added individual button icons and changes app and splah icons

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
         return (
             <View style={styles.HomeScreen}>
                 <TouchableOpacity onPress={() => navigation.navigate('Photos')}>
+                    <Image style={styles.Icon} source={require('./assets/photos.png')}/>
                     <Text>Browse Photos</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => navigation.navigate('Collections')}>
+                    <Image style={styles.Icon} source={require('./assets/collection.png')}/>
                     <Text>Browse Collections</Text>
                 </TouchableOpacity>
             </View>
@@ -16,6 +18,12 @@ export default function HomeScreen({ navigation }) {
 
 const styles = StyleSheet.create({
     HomeScreen: {
-        padding: 20
+        padding: 20,
+        flexDirection: "row",
+        columnGap: 30
     },
+    Icon: {
+        width: 100,
+        height: 100
+    }
 });
